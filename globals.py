7c4e5e1d4fcb5d6e30650e10441e988464aa071a
Bohrium107 <ernesla77@gmail.com>
2025-07-26 16:27:21 +0200

    Add editor for nonogram grids

--- globals.py
+++ globals.py
@@ -1,1 +1,44 @@
+import pygame as pg
+
 bg_color = (8, 6, 10)
+
+class InputBox():
+    font = None
+    BOX_COLOR = (80, 80, 80)
+    BOX_COLOR_LIGHT = (110, 110, 110)
+
+    def __init__(self, position, tooltip, tag='', width=300, height=80):
+        super().__init__()
+        self.image = pg.Surface((width, height))
+        self.rect = self.image.get_rect(center=position)
+        self.font = pg.font.Font(None, 25)
+        self.text = ''
+        self.tag = tag
+        self.tooltip = tooltip
+
+        self._redraw()
+
+    def _redraw(self):
+        self.image.fill(pg.Color(self.BOX_COLOR))
+        self.image.fill(pg.Color(self.BOX_COLOR_LIGHT), (0, 0, self.rect.width, 30))
+
+        tooltip_surf = self.font.render(self.tooltip, True, 'white')
+        self.image.blit(tooltip_surf, (10, 7))
+
+        text_surf = self.font.render(self.text, True, 'white')
+        text_rect = pg.Rect(0, 0, self.rect.width - 16, 28)
+        text_rect.midleft = (14, self.rect.height - 20)
+        self.image.blit(text_surf, text_rect)
+
+        input_field_rect = pg.Rect(0, 0, self.rect.width - 16, 28)
+        input_field_rect.midleft = (8, self.rect.height - 25)
+        pg.draw.rect(self.image, self.BOX_COLOR_LIGHT, input_field_rect, width=2)
+
+    def on_text_input(self, event):
+        self.text += event.text
+        self._redraw()
+
+    def on_key_down(self, event):
+        if event.key == pg.K_BACKSPACE:
+            self.text = self.text[:-1]
+            self._redraw()
---
+++ nonogram/editor.py
@@ -0,0 +1,231 @@
+import pygame as pg
+from itertools import groupby
+import json
+
+from globals import InputBox
+
+
+class Grid:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.row_guides = self._get_row_guides()
+        self.col_guides = self._get_col_guides()
+
+    # Returns a list of lists of numbers representing the consecutive black boxes in each row
+    def _get_row_guides(self):
+        guides = []
+
+        for i in range(len(self.matrix)):
+            nums = [len(list(group)) for key, group in groupby(self.get_row(i)) if key == 1]
+
+            if not nums:
+                guides.append([0])
+            else:
+                guides.append(nums)
+
+        return guides
+
+    # Returns a list of lists of numbers representing the consecutive black boxes in each column
+    def _get_col_guides(self):
+        guides = []
+
+        for i in range(len(self.matrix)):
+            nums = [len(list(group)) for key, group in groupby(self.get_col(i)) if key == 1]
+
+            if not nums:
+                guides.append([0])
+            else:
+                guides.append(nums)
+
+        return guides
+
+    # Returns the i-th column
+    def get_col(self, i):
+        return list(map(lambda row: row[i], self.matrix))
+
+    # Returns the i-th row
+    def get_row(self, i):
+        return self.matrix[i]
+
+    # Get the state of a box
+    def get_box(self, row, col):
+        return self.matrix[row][col]
+
+    # Set the state of a box
+    def set_box(self, row, col, state):
+        self.matrix[row][col] = state
+        self.row_guides = self._get_row_guides()
+        self.col_guides = self._get_col_guides()
+
+def draw_grid():
+    # Draw grid boxes
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            box_rect = (grid_rect.x + j * BOX_LENGTH, grid_rect.y + i * BOX_LENGTH, BOX_LENGTH, BOX_LENGTH)
+
+            if grid.get_box(i, j) == 0:
+                pg.draw.rect(screen, GRID_BG_COL, box_rect)
+            elif grid.get_box(i, j) == 1:
+                pg.draw.rect(screen, GRID_BORDER_COL, box_rect)
+            elif grid.get_box(i, j) == 2:
+                pg.draw.rect(screen, pg.Color('red'), box_rect)  # TODO: Placeholder
+
+    # Draw grid lines
+    for i in range(GRID_SIZE - 1):
+        # Vertical line
+        pg.draw.line(screen, LINE_COL,
+                     (grid_rect.x + (i + 1) * BOX_LENGTH, grid_rect.y),
+                     (grid_rect.x + (i + 1) * BOX_LENGTH, grid_rect.y + GRID_SIZE * BOX_LENGTH - 1))
+        # Horizontal line
+        pg.draw.line(screen, LINE_COL,
+                     (grid_rect.x, grid_rect.y + (i + 1) * BOX_LENGTH),
+                     (grid_rect.x + GRID_SIZE * BOX_LENGTH - 1, grid_rect.y + (i + 1) * BOX_LENGTH))
+
+    for i in range(4, GRID_SIZE - 1, 5):
+        # Vertical thick line
+        pg.draw.line(screen, THICK_LINE_COL,
+                     (grid_rect.x + (i + 1) * BOX_LENGTH, grid_rect.y),
+                     (grid_rect.x + (i + 1) * BOX_LENGTH, grid_rect.y + GRID_SIZE * BOX_LENGTH - 1), 3)
+        # Horizontal thick line
+        pg.draw.line(screen, THICK_LINE_COL,
+                     (grid_rect.x, grid_rect.y + (i + 1) * BOX_LENGTH),
+                     (grid_rect.x + GRID_SIZE * BOX_LENGTH - 1, grid_rect.y + (i + 1) * BOX_LENGTH), 3)
+
+    # Draw grid border
+    pg.draw.rect(screen, GRID_BORDER_COL,
+                 (grid_rect.x, grid_rect.y, BOX_LENGTH * GRID_SIZE, BOX_LENGTH * GRID_SIZE), 3)
+
+def on_mouse_down(e):
+    global pressed
+    pressed = e.button
+
+def on_mouse_up():
+    global pressed, first_changed_state
+    pressed = 0
+    first_changed_state = None
+
+def on_mouse_pressed():
+    mousex, mousey = pg.mouse.get_pos()
+    col_clicked = (mousex - grid_rect.x) / BOX_LENGTH
+    row_clicked = (mousey - grid_rect.y) / BOX_LENGTH
+
+    toggle_map = {
+        (1, 0): 1,
+        (1, 1): 0
+    }
+
+    if 0 < col_clicked < GRID_SIZE and 0 < row_clicked < GRID_SIZE:  # if mouse on grid
+        row = int(row_clicked)
+        col = int(col_clicked)
+        box_state = grid.get_box(row, col)
+
+        global first_changed_state
+        if first_changed_state is None:
+            first_changed_state = box_state
+
+        # Only change state if this box has the same state as the box changed first with this mouse press
+        if (pressed, box_state) in toggle_map and box_state == first_changed_state:
+            grid.set_box(row, col, toggle_map[(pressed, box_state)])
+
+def load_grid(name):
+    global BOX_LENGTH, GRID_SIZE, grid, grid_rect, grid_created
+
+    with open(f'grids/{name}.json') as file:
+        grid = Grid(json.load(file))
+
+    GRID_SIZE = len(grid.matrix)
+    BOX_LENGTH = int((screen.width - MARGIN_BOTTOMRIGHT - MARGIN_TOPLEFT) / GRID_SIZE)
+    grid_rect = pg.Rect(MARGIN_TOPLEFT, MARGIN_TOPLEFT, BOX_LENGTH * GRID_SIZE, BOX_LENGTH * GRID_SIZE)
+    grid_created = True
+
+def create_empty_grid(size):
+    global BOX_LENGTH, GRID_SIZE, grid, grid_rect, grid_created
+    GRID_SIZE = size
+    BOX_LENGTH = int((screen.width - MARGIN_BOTTOMRIGHT - MARGIN_TOPLEFT) / size)
+
+    grid = Grid([[0] * size for _ in range(size)])
+    grid_rect = pg.Rect(MARGIN_TOPLEFT, MARGIN_TOPLEFT, BOX_LENGTH * size, BOX_LENGTH * size)
+    grid_created = True
+
+def save_grid(name):
+    with open(f'grids/{name}.json', 'w') as file:
+        json.dump(grid.matrix, file)
+
+# Setup
+pg.init()
+pg.display.set_caption('Nonogram')
+screen = pg.display.set_mode((700, 700))
+running = True
+delta_time = 0.1
+clock = pg.time.Clock()
+
+MARGIN_BOTTOMRIGHT = 90
+MARGIN_TOPLEFT = 90
+
+pressed = 0  # Mouse button currently being pressed
+first_changed_state = None  # State of the box over which the current mouse press began
+grid_created = False
+
+# Colors
+GRID_BG_COL = (190, 190, 190)
+LINE_COL = (150, 150, 150)
+THICK_LINE_COL = (130, 130, 130)
+GRID_BORDER_COL = (10, 10, 10)
+
+input_box = InputBox(screen.get_rect().center, 'Size of the grid:', 'create_grid')
+
+while running:
+    screen.fill(pg.color.Color('white'))
+
+    if grid_created:
+        draw_grid()
+
+    if input_box:
+        screen.blit(input_box.image, input_box.rect)
+
+    screen.blit(pg.Font(None, 25).render(
+        'r to create a new grid, s to save grid to a file, l to load a grid from a file, \nESC to close input box', True, 'black'), (20, 20))
+
+    delta_time = clock.tick(60) / 100
+    delta_time = min(1.0, delta_time)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN:
+            if input_box:
+                input_box.on_key_down(event)
+
+            if event.key == pg.K_RETURN and input_box:
+                if input_box.tag == 'create_grid':
+                    create_empty_grid(int(input_box.text))
+                elif input_box.tag == 'save_grid':
+                    save_grid(input_box.text)
+                elif input_box.tag == 'load_grid':
+                    load_grid(input_box.text)
+                input_box = None
+
+            elif event.key == pg.K_r and not input_box:
+                input_box = InputBox(screen.get_rect().center, 'Size of the new grid:', 'create_grid')
+            elif event.key == pg.K_s and not input_box:
+                input_box = InputBox(screen.get_rect().center, 'Save grid as:', 'save_grid')
+            elif event.key == pg.K_l and not input_box:
+                input_box = InputBox(screen.get_rect().center, 'Name of the grid to load:', 'load_grid')
+            elif event.key == pg.K_ESCAPE:
+                input_box = None
+        elif event.type == pg.TEXTINPUT:
+            if input_box:
+                input_box.on_text_input(event)
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if grid_created:
+                on_mouse_down(event)
+        elif event.type == pg.MOUSEBUTTONUP:
+            if grid_created:
+                on_mouse_up()
+
+    pg.display.flip()
+
+    if pressed:
+        on_mouse_pressed()
+
+pg.quit()
